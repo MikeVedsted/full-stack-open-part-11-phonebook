683bf33793fdfb96c5015a4121898b1abdea2705
Mike Vedsted <mikevedsted@gmail.com>
2021-03-09 16:27:19 +0200

    Test static serving

--- index.js
+++ index.js
@@ -84,6 +84,10 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'))
+// })
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
